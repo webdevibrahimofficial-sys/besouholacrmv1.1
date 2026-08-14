8cbd6a2c1f77ad262fabae14bb01d1c7c044444a
oi328 <oi328@users.noreply.github.com>
2026-08-14 16:33:41 +0300

    Remove production debug artifacts

--- temp_list_tenants.php
+++
@@ -1,7 +0,0 @@
-<?php
-require '/var/www/vendor/autoload.php';
-$app = require '/var/www/bootstrap/app.php';
-$kernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
-$kernel->bootstrap();
-$tenants = App\Models\Tenant::query()->orderBy('id')->get(['id','name','slug','status']);
-echo json_encode($tenants, JSON_PRETTY_PRINT) . PHP_EOL;
--- temp_make_token.php
+++
@@ -1,8 +0,0 @@
-<?php
-require '/var/www/vendor/autoload.php';
-$app = require '/var/www/bootstrap/app.php';
-$kernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
-$kernel->bootstrap();
-$u = App\Models\User::withoutGlobalScopes()->find(1);
-$token = $u->createToken('debug_quick_switch');
-echo $token->plainTextToken . PHP_EOL;
